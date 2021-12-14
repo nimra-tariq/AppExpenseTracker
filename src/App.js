c0b9eb7ff2507bcb72d46401c34e95ac0b8926c6
nimra tariq <nimratariq899@gmail.com>
2021-12-14 13:21:55 +0500

    initial setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <h1 className='container'>expense tracker app by nimra tariq</h1>
+  </>
+  );
+}
+
+export default App;
